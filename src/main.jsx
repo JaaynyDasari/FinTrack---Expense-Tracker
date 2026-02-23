5b69db8914d84e02a088dc820d9bff3efcc5a969
JaaynyDasari <jaaynydasari@gmail.com>
2026-02-23 12:37:57 +0530

    Resolved merge conflict

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 import { HashRouter } from "react-router-dom";
-=======
-import { BrowserRouter } from 'react-router-dom'
->>>>>>> 72fd91e65eeb53585deee7fd9b6d39ab6b2915c6
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import 'react-datepicker/dist/react-datepicker.css'
@@ -14,16 +10,9 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<<<<<<< HEAD
 <HashRouter>
         <App />
       <ToastContainer position="bottom-right" autoClose={3000} />
     </HashRouter>
-=======
-    <BrowserRouter>
-      <App />
-      <ToastContainer position="bottom-right" autoClose={3000} />
-    </BrowserRouter>
->>>>>>> 72fd91e65eeb53585deee7fd9b6d39ab6b2915c6
   </StrictMode>,
 )
